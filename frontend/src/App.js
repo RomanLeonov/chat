c246712c856674e41ebdb1ac557d28d84c86b9ca
Roman Leonov <roman.leonov@duallab.com>
2019-03-26 13:17:28 +0300

    Init frontend side

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         Hi Stas, you should work here!
+      </div>
+    );
+  }
+}
+
+export default App;
